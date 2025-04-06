237485dec97159272516ae113f92b5c13c4bd9dd
Connor <connor@51e90d0cd16c>
2025-04-06 01:23:19 +0000

    deleted obsolete file

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 import sys
 
+# PARSER - puts lines of pyspark job into a list
 # Get job name from user
-
 try:
     jobName = sys.argv[1]
 except: sys.exit("INPUT ERROR: must specify a job name from jobs folder ending in .py")
-
-# User specifies a string for job name
 job = open("./jobs/"+jobName, "r")
 lines = [line.rstrip() for line in job]
+# END PARSER
+
+
+# MAIN LOGIC - loop through the lines
+for i in range(len(lines)):
+    # This is where we need to start associating line number with operations
