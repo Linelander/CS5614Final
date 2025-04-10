ef65dc4e21e00232b05bb73d64660b782a270b00
Connor Burch <connorb24@vt.edu>
2025-04-09 20:03:10 -0400

    working inheritance and arithmetic. no mechanism for automatically getting line numbers

---
+++ ArithmeticTest.py
@@ -0,0 +1,40 @@
+import StampMath
+import operator
+
+y = StampMath.arithmetic(777, operator.add, 1, 7, 5)
+print("add (expect 13): " + str(y) + "\n")
+
+y = StampMath.arithmetic(777, operator.truediv, 6, 3, 5)
+print("true division (expect 0.4): " + str(y) + "\n")
+
+y = StampMath.arithmetic(777, operator.mul, 6, 3, 5)
+print("multiplication (expect 90): " + str(y) + "\n")
+
+y = StampMath.arithmetic(777, operator.sub, 6, 3, 5)
+print("subtraction (expect -2): " + str(y) + "\n")
+
+# Instantiate a stamped value
+three = StampMath.StampedValue(3)
+three.line_numbers = [17] # line number of creation. we'll automate this later
+
+# NOTE: probably need to get the line number where it's created too
+
+print("GENEALOGY TEST")
+print("--------------")
+a = StampMath.arithmetic(22, operator.sub, 6, 8, 5)
+print("subtraction with stamped middle (expect -7): " + str(a.value) + "\n")
+# a = -7
+
+b = StampMath.arithmetic(25, operator.sub, 3, a, 6)
+print("subtraction 2 (expect 4): " + str(b.value) + "\n")
+# b = 4
+
+c = StampMath.arithmetic(28, operator.sub, b, 6, 5)
+print("subtraction 2 starting with stamped (expect -7): " + str(c.value) + "\n")
+# c = -7
+
+d = StampMath.arithmetic(31, operator.sub, 9, 7, c)
+print("subtraction on only stamped numbers (expect 9): " + str(d.value) + "\n")
+# d = 9
+
+print("Genealogy of stamped value named d: " + str(d.line_numbers))
--- StampMath.py
+++ StampMath.py
@@ -1,52 +1,32 @@
 import operator
 
-
 class StampedValue:
-    def __init__(self, val, loc):
+    def __init__(self, val):
         self.value = val
-        self.line_number = loc
+        self.line_numbers = []
 
-
-
-
-
-def stamp_math(lineNo, operation, *args):
+def arithmetic(lineNo, operation, *args):
+    # empty list lines inherits line numbers of all number arguments
+    lines = []
     if type(args[0]) == StampedValue:
         x = args[0].value
+        lines += [num for num in args[0].line_numbers]
     else:
         x = args[0]
+    
     i = 1
     while i < len(args):
         if type(args[i]) == StampedValue:
             x = operation(x, args[i].value)
+            lines += [num for num in args[i].line_numbers]
         else:
             x = operation(x, args[i])
         i+=1
-    return x
+    
+    # add the line number this function was called
+    lines += [lineNo]
 
-
-y = stamp_math(777, operator.add, 1, 7, 5)
-print("add (expect 13): " + str(y) + "\n")
-
-y = stamp_math(777, operator.truediv, 6, 3, 5)
-print("true division (expect 0.4): " + str(y) + "\n")
-
-y = stamp_math(777, operator.mul, 6, 3, 5)
-print("multiplication (expect 90): " + str(y) + "\n")
-
-y = stamp_math(777, operator.sub, 6, 3, 5)
-print("subtraction (expect -2): " + str(y) + "\n")
-
-three = StampedValue(3, -1)
-
-y = stamp_math(777, operator.sub, 6, three, 5)
-print("subtraction with stamped middle (expect -2): " + str(y) + "\n")
-
-y = stamp_math(777, operator.sub, 3, 6, 5)
-print("subtraction 2 (expect -8): " + str(y) + "\n")
-
-y = stamp_math(777, operator.sub, three, 6, 5)
-print("subtraction 2 starting with stamped (expect -8): " + str(y) + "\n")
-
-y = stamp_math(777, operator.sub, three, three, three)
-print("subtraction on only stamped numbers (expect -3): " + str(y) + "\n")
+    # Stamp x with [lines]
+    final = StampedValue(x)
+    final.line_numbers = lines
+    return final
