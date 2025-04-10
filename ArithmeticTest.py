7621a79bb32e007e309b81ad892db73b88d7b66b
Connor Burch <connorb24@vt.edu>
2025-04-09 20:50:35 -0400

    another typo fix

--- ArithmeticTest.py
+++ ArithmeticTest.py
@@ -22,11 +22,11 @@ three.line_numbers = [17] # line number of creation. we'll automate this later
 print("GENEALOGY TEST")
 print("--------------")
 a = StampMath.arithmetic(24, operator.sub, 6, 8, 5)
-print("subtraction with stamped middle (expect -7): " + str(a.value) + "\n")
+print("integer subtraction (expect -7): " + str(a.value) + "\n")
 # a = -7
 
 b = StampMath.arithmetic(28, operator.sub, 3, a, 6)
-print("subtraction 2 (expect 4): " + str(b.value) + "\n")
+print("subtraction with stamped middle (expect 4): " + str(b.value) + "\n")
 # b = 4
 
 c = StampMath.arithmetic(32, operator.sub, b, 6, 5)
@@ -34,7 +34,7 @@ print("subtraction 2 starting with stamped (expect -7): " + str(c.value) + "\n")
 # c = -7
 
 d = StampMath.arithmetic(36, operator.sub, 9, 7, c)
-print("subtraction on only stamped numbers (expect 9): " + str(d.value) + "\n")
+print("subtraction with stamp at end (expect 9): " + str(d.value) + "\n")
 # d = 9
 
 print("Genealogy of stamped value named d: " + str(d.line_numbers))
