f80bc6b38dc9ea5e2000b802ad6090ad4271fe85
Connor <connor@51e90d0cd16c>
2025-04-25 18:44:59 +0000

    skeleton

--- StampMath.py
+++ StampMath.py
@@ -1,6 +1,7 @@
 import operator
 import inspect
 
+# Will hold an RDD and relevant line numbers
 class StampedValue:
     def __init__(self, val, line_num):
         self.value = val
@@ -40,23 +41,27 @@ def arithmetic(operation, *args):
 # Arbitrate one to many pyspark methods
 # NOTE: this does not account for data in the RDD already being stamped. need to account for that in the stampedvalue constructor
 def oneToMany(resilient, methodstr, *args):
+    lines_list = []
+    
     # Get line number of caller
     frame = inspect.currentframe()
     caller_frame = frame.f_back
     line_num = caller_frame.f_lineno
 
     # TODO: get line numbers of resilient
-    rdd_lines = []
     def accrueLines(stamped):
-        rdd_lines += stamped.line_numbers
+        lines_list += stamped.line_numbers
     resilient.foreach(accrueLines)
 
+    # TODO: get a normal rdd (if input is stamped)
+    unstamped = resilient.map(lambda x: (x.val))
+
     # TODO: Apply the user supplied method. NOTE: use getattr()
-    method = # ...
-    original = # ...
+    method = getattr(resilient, methodstr)
+    original = method(args)
 
     # TODO: Stamp all line numbers on the data
-    lines_list = rdd_lines += [line_num]
+    lines_list = [line_num]
     return original.map(lambda x: (StampedValue(x, lines_list)))
 
 
