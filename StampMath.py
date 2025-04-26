6e945740807adaa41e2cbe651e6b7d22f6033c18
Connor <connor@51e90d0cd16c>
2025-04-26 00:25:09 +0000

    saving progress

--- StampMath.py
+++ StampMath.py
@@ -38,14 +38,65 @@ def arithmetic(operation, *args):
     return final
 
 
-# This method can stamp asymmetric operations, but only if every single line number in the RDD should be included in the output.
-# It needs to be improved to be more aware of where the things contained in RDDs get their data from.abs
-# What if you had an RDD with data having these line numbers
+
+def rddArithmetic(operation, resilient):
+    # empty list lines inherits line numbers of all number arguments
+    args = resilient.collect()
+    lines = []
+    
+    frame = inspect.currentframe()
+    caller_frame = frame.f_back
+    line_num = caller_frame.f_lineno
+    
+    if type(args[0]) == StampedValue:
+        x = args[0].value
+        lines += [num for num in args[0].line_numbers]
+    else:
+        x = args[0]
+    
+    i = 1
+    while i < len(args):
+        if type(args[i]) == StampedValue:
+            x = operation(x, args[i].value)
+            lines += [num for num in args[i].line_numbers]
+        else:
+            x = operation(x, args[i])
+        i+=1
+    
+    # add the line number this function was called
+    lines += [line_num]
+
+    # Stamp x with [lines]
+    final = StampedValue(x, lines)
+    return final
+
+
+# run an operatoin functionally with no tainting. Use in tandem with arithemetic()
+def functionalOp(resilient, methodstr, *args):
+    method = getattr(resilient, methodstr)
+    return method(*args)
+
+
+# IMPORTANT TODO:
+# This method can stamp asymmetric operations, but only if every single line number in the RDD should be included in all
+# tuples of the output.
+#
+# What if you had an RDD with data having these line numbers:
+#
 # (a,[1,3]) (a,[5,7]) (b,[9,11])
+#
 # And then you did some by-key operation?
+#
 # This function in its current state would not realize that the tuple resulting in the operation carried out on all tuples
-# having the key a shouldn't contain b's line numbers 9 & 11.
-def asymOperation(resilient, methodstr, *args):
+# having the key a shouldn't contain b's line numbers 9 & 11. It also doesn't realize that things resulting from b shouldn't
+# have line numbers associated with a.
+#
+# CORRECT OUTPUT (our goal): [(a+a,[1,3,5,7]), (b,[9,11])]
+# OUR OUTPUT (incorrect): [(a+a,[1,3,5,7,9,11]), (b,[1,3,5,7,9,11])]
+#
+# Method stub for solution after this method
+#
+def asymOp(resilient, methodstr, *args):
     lines_list = []
     
     # Get line number of caller
@@ -66,3 +117,23 @@ def asymOperation(resilient, methodstr, *args):
     original = method(*args)
     lines_list += [line_num]
     return original.map(lambda x: (StampedValue(x, lines_list)))
+
+
+# Perform an operation by key. Only spread taint within keys.
+# def opByKey(resilient, methodstr, *args):
+    # 1. Unstamp input
+    # 2. Group it
+    # 3. Split it
+    # 4. perform operation
+    # 5. stamp
+    # 6. recombine
+
+
+
+# Stamp everything in a vanilla RDD with the caller line number (use for SparkContext.textFile() and the like)
+def stampRDD(resilient):
+    frame = inspect.currentframe()
+    caller_frame = frame.f_back
+    line_num = caller_frame.f_lineno
+    
+    return resilient.map(lambda x: StampedValue(x, [line_num]))
--- rddtest.py
+++ rddtest.py
@@ -14,37 +14,29 @@ spark = SparkSession.builder \
 
 sc = spark.sparkContext
 
-text = sc.textFile("./data/words.txt")
+text = StampMath.stampRDD(sc.textFile("./data/words.txt"))
 
 # words = text.flatMap(lambda line: line.split(" "))
 # words1 = words
 # print(words1.collect())
 
-words = StampMath.asymOperation(text, 'flatMap', lambda line: line.split(" "))
+words = StampMath.asymOp(text, 'flatMap', lambda line: line.split(" "))
 print("values from 1: " + str(words.map(lambda x: (x.value)).collect()))
 print("lines from 1: " + str(words.map(lambda x: (x.line_numbers)).collect()))
 
 print("--------------------------")
 
 # .map(lambda word: (word, 1))
-words2 = StampMath.asymOperation(words, "map", lambda word: (word, 1))
+words2 = StampMath.asymOp(words, "map", lambda word: (word, 1))
 print("values from 2: " + str(words2.map(lambda x: (x.value)).collect()))
 print("lines from 2: " + str(words2.map(lambda x: (x.line_numbers)).collect()))
 
 
-# stamped_words2 = StampMath.asymOperation(words, "map()", lambda word: (word, 1))
+
+# counts = words2.reduceByKey(lambda x, y: StampMath.arithmetic(operator.add, x, y))
 
 
+counts = StampMath.asymOp(words2, "reduceByKey", lambda x, y: (StampMath.arithmetic(operator.add, x, y)))
 
-
-
-# counts = words.reduceByKey(operator.add)
-
-# # counts = StampMath.arithmetic
-
-
-
-
-
-
-# print(counts.collect())
+print("values final: " + str(counts.map(lambda x: (x.value)).collect()))
+print("lines final: " + str(counts.map(lambda x: (x.line_numbers)).collect()))
