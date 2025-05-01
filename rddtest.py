bb673f07023df1143b147a76bb24017cd9f5079a
Connor <connor@51e90d0cd16c>
2025-05-01 22:44:10 +0000

    removed safegaurds in stampMap so it behaves more like vanilla pyspark

--- rddtest.py
+++ rddtest.py
@@ -20,7 +20,7 @@ print("stage1:", str(words.collect()))
 
 print("--------------------------")
 
-words2 = StampMath.oneToOne(words, "map", lambda word: (word, 1))
+words2 = StampMath.stampMap(words, "map", lambda word: (word, 1))
 print("stage2:", str(words2.collect()))
 
 print("--------------------------")
@@ -82,18 +82,41 @@ rdd2 = StampMath.stampNewRDD(sc.parallelize([('dog', 1), ('cat', 1), ('dog', 1)]
 grouped2 = StampMath.manyToMany(rdd2, "groupByKey")
 print("SEMIFINAL")
 print(grouped2.collect())
-result2 = StampMath.oneToOne(grouped2, "map", lambda kv: (kv[0], len(kv[1])))
+result2 = StampMath.stampMap(grouped2, "map", lambda kv: (kv[0], len(kv[1])))
 print("FINAL")
 print(result2.collect())
 
-# --- Harder test: single RDDs containing data from different places ---
-# JOIN TEST
 
-# one = StampMath.stampRDD(sc.parallelize(['a', 'a', 'a', 'b', 'c']).map(lambda x: (x, 1)))
-# two = StampMath.stampRDD(sc.parallelize(['b', 'b', 'b', 'a']).map(lambda x: (x, 1)))
 
-# # TODO: need a way to handle joins
-# # joined = # TODO
 
-# print("values join: " + str(joined.map(lambda x: (x.value)).collect()))
-# print("lines join: " + str(joined.map(lambda x: (x.line_numbers)).collect()))
+print("----------- REGULAR FLATMAP TEST ----------")
+rdd2 = sc.parallelize(['str', 'abc', '123'])
+# print(rdd2.flatMap(lambda x: range(1, x)).collect())
+# [1, 1, 1, 2, 2, 3]
+print(rdd2.flatMap(lambda x: [(x, x), (x, x)]).collect())
+# [(2, 2), (2, 2), (3, 3), (3, 3), (4, 4), (4, 4)]
+print()
+
+print("----------- MODDED FLATMAP TEST ----------")
+rdd3 = StampMath.stampNewRDD(sc.parallelize(['str', 'abc', '123']))
+# print(StampMath.stampMap(rdd3, "flatMap", lambda x: range(1, x)).collect())
+print(StampMath.stampMap(rdd3, "flatMap", lambda x: [(x, x), (x, x)]).collect())
+
+print()
+
+
+
+# NOTE: it seems like an error, but recall vanilla pyspark flatmap messes up strings. Our version needs to do that too.
+# faithful to the original
+print("------------ STRING CHOPPING TEST ---------------")
+rdd = sc.parallelize(['alpha', 'beta'])
+stamped = StampMath.stampNewRDD(rdd)
+chopped = StampMath.stampMap(stamped, "flatMap", lambda x: x.upper())
+print(chopped.collect())
+
+print()
+
+print("------------ REGULAR STRING CHOPPING TEST ---------------")
+rdd = sc.parallelize(['alpha', 'beta'])
+result = rdd.flatMap(lambda x: x.upper())
+print(result.collect())
