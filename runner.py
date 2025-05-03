03379904144c96c2dc90c50003d719683116b216
aaronho0216 <113267096+aaho0216@users.noreply.github.com>
2025-05-02 23:28:26 -0400

    update runner file

--- runner.py
+++ runner.py
@@ -15,7 +15,7 @@ line_number_list = [list(map(int, match.split(','))) for match in matches]
 
 # getting suspicious line numbers
 testDataResults = []
-faulty_lines = {23, 81} # can change this to any line number(s)
+faulty_lines = {104} # can change this to any line number(s)
 
 for i, line_list in enumerate(line_number_list):
     passed = not any(line in faulty_lines for line in line_list)
